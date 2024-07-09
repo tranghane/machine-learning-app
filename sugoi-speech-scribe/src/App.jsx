4ed576ba76540b10625f729dc84508a348d8b67a
Your Name <trungduongnguyen2004@gmail.com>
2024-07-08 21:38:51 -0700

    Initial set up

---
+++ sugoi-speech-scribe/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-green-400'>This is the start of the project</h1>
+    </>
+  )
+}
+
+export default App
