2592b644b81b21f01809ff35b06c1596a1545af8
Your Name <trungduongnguyen2004@gmail.com>
2024-07-10 14:32:07 -0700

    startRecording function finished (maybe)

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 export default function HomePage(props) {
   const { setAudioStream, setFile } = props; //get the props as function
+
+  //some State variables
+  const [recordingStatus, setRecordingStatus] = useState('inactive')
+  const [audioChunks, setAudioChunks] = useState ([])
+  const [duration, setDuration] = useState(0)
+
+  const mediaRecorder = useRef(null)
+  const mimeType = 'audio/webm' //mimeType for media recorder
+
+  async function startRecording() {
+    let tempStream
+    console.log('Start recording')
+
+    //checks if the "MediaRecorder" object is available in the browser window.
+    try {
+      // request permission to access the user’s microphone
+      const streamData = navigator.mediaDevices.getUserMedia({
+        audio: true,  
+        video: false
+      })
+      tempStream = streamData
+    }catch (err) {
+      console.log(err.message)
+      return
+    }
+
+    //create new media recorder instance using the stream
+    const media = new MediaRecorder(tempStream, {type: mimeType})
+
+    mediaRecorder.current = media //assign the recorder
+
+    //start recording
+    mediaRecorder.current.start()
+    let localAudioChunks = []
+
+    //ondataavailable event, fires when audio data becomes available during recording
+    mediaRecorder.current.ondataavailable = (event) => {
+      //case where there's no data
+        if (typeof event.data === 'undefined') {return}
+        if (event.data.size === 0) {return}
+        localAudioChunks.push(event.data)
+    }
+    setAudioChunks(localAudioChunks)
+  }
   return (
     <main
       className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5
