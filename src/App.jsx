461f86cbf5e8372c8416d6639303c5c71bef073a
Your Name <trungduongnguyen2004@gmail.com>
2024-07-11 22:44:05 -0700

    trascribing looks good

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState, useEffect } from 'react'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
 import FileDisplay from './components/FileDisplay'
+import Information from './components/Information'
+import Transcribing from './components/Transcribing'
 function App() {
   // 2 variable for upload a file or record
   const [file, setFile] = useState(null)
   const [audioStream, setAudioStream] = useState(null)
+  const [output, setOutput] = useState(null)
+  const [loading, setLoading] = useState(true)
 
 
   //check if there's any thing, either file or record audio
@@ -26,9 +30,16 @@ function App() {
     mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header/>
-        {isAudioAvailable ? ( //if there's audio, display files
+        {output ? (
+          <Information/>
+        ) : loading ? (
+          <Transcribing/>
+        ) : isAudioAvailable ? (
           <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={audioStream}/>
-        ) : (<HomePage setFile={setFile} setAudioStream={setAudioStream}/>)} 
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream}/>
+        )}
+
         {/* pass in setters as props */}
 
       </section>
