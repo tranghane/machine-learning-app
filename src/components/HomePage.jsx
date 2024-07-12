cc1cfa105d96b4012fd1784442612a6806cd8742
Your Name <trungduongnguyen2004@gmail.com>
2024-07-11 21:44:59 -0700

    start and stop record UI worked

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -30,6 +30,8 @@ export default function HomePage(props) {
       return
     }
 
+    setRecordingStatus('recording')
+
     //create new media recorder instance using the stream
     const media = new MediaRecorder(tempStream, {type: mimeType})
 
@@ -50,8 +52,36 @@ export default function HomePage(props) {
   }
 
   async function stopRecording() {
-    
+    //notify that stop recording
+    setRecordingStatus('inactive')
+    console.log('Stop recording')
+
+
+    mediaRecorder.current.stop()
+    mediaRecorder.current.onstop = () => {
+      //creating a blob from the audio chunks
+      const audioBlob = new Blob(audioChunks, {type: mimeType})
+      setAudioStream(audioBlob)
+      setAudioChunks([]) //reset to default value
+      setDuration(0)
+    }
+
   }
+
+  //to see how long user have recorded
+  useEffect(() => {
+    if (recordingStatus === 'inactive') {return}
+
+    //interval increase every seconds
+    const interval = setInterval(() => {
+      setDuration(curr => curr + 1) //keep increasing
+    }, 1000)
+
+    return () => clearInterval(interval)
+
+  }) 
+
+
   return (
     <main
       className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5
@@ -68,12 +98,18 @@ export default function HomePage(props) {
         Translate
       </h3>
       <button
+        onClick={recordingStatus === 'recording' ? stopRecording : startRecording}
         className="flex items-center text-base justify-between 
                                 gap-4 mx-auto w-72 max-w-full my-4
                                 specialBtn px-4 py-2 rounded-xl"
       >
-        <p className="text-blue-400">Record</p>
-        <i className="fa-solid fa-microphone-lines"></i>
+        <p className="text-blue-400">{recordingStatus === 'inactive' ? 'Record' : 'Stop recording'}</p>
+        <div className="flex items-center gap-2">
+          {duration && (
+            <p className="text-sm "> {duration}s</p>
+          )}
+          <i className={"fa-solid fa-microphone-lines duration-200 " + (recordingStatus === 'recording' ? ' text-rose-300' : ' ')}></i>
+        </div>
       </button>
       <p className="text-base">
         Or{" "}
